89fc998ef6926f43cdedba301b7e79756544df2f
khaled <awadkhaled.webdev@gmail.com>
2022-12-27 22:06:06 +0300

    step 3

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+
 app.get('/', (req, res) => {
     res.send('OK');
   });
+
+  app.get('/test', (req, res) => {
+    res.json({status: 200, message: "ok"});
+  });
+
+  app.get('/time', (req, res) => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    res.json({status: 200, message: `${hours}:${minutes}`});
+  });
   
+  app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
